2fbd32cf37bddd54acbdc32fc864f583daa9cb3c
Victor Olofsson <victorolofsson@hotmail.com>
2020-09-14 14:05:48 +0200

    Solved answer for 42

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 #1
-=======
 #2
->>>>>>> second
 
 def main():
     for n in range(1, 101):
         if n % 3 == 0 or n % 5 == 0:
             if n % 3 == 0 and n % 5 == 0:
                 print("FizzBuzz")
+            elif n == 42:
+                print("Answer to the Ultimate Question of Life, the Universe, and Everything")
             elif n % 3 == 0:
                 print("Fizz")
             else:
